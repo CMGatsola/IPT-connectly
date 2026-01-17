47bc58a1c931a8276b39a278a8f3cffbbc7a4e40
unknown <lr.cmgatsola@mmdc.mcl.edu.ph>
2026-01-17 08:03:28 +0800

    first commit

---
+++ posts/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin # type: ignore due to env not on
+
+# Register your models here.
---
+++ posts/tests.py
@@ -0,0 +1,3 @@
+from django.test import TestCase # type: ignore due to env not on
+
+# Create your tests here.
